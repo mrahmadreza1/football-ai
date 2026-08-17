07714153f242ae4e742049bbd924e066a2869598
ahmadreza <menatiahmadreza@gmail.com>
2026-08-17 03:45:24 +0330

    change analyzer model

--- app/ai_editor.py
+++ app/ai_editor.py
@@ -31,7 +31,7 @@ Rules:
 """
 
     response = client.chat.completions.create(
-        model="openai/gpt-oss-120b",
+        model="llama-3.3-70b-versatile",
         messages=[
             {
                 "role": "user",
