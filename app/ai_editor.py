9eb536a75ecc3207083354e1c515df71e51825ae
ahmadreza <menatiahmadreza@gmail.com>
2026-08-17 04:00:24 +0330

    change analyzer model

--- app/ai_editor.py
+++ app/ai_editor.py
@@ -31,7 +31,7 @@ Rules:
 """
 
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="llama-3.1-8b-instant",
         messages=[
             {
                 "role": "user",
