d4b88badbc5918b512fb38c602e26c2af28ded55
ahmadreza <menatiahmadreza@gmail.com>
2026-08-17 04:34:39 +0330

    change analyzer model

--- app/translator.py
+++ app/translator.py
@@ -45,7 +45,7 @@ DESCRIPTION:
 """
 
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="llama-3.1-8b-instant",
         messages=[
             {
                 "role": "user",
