92bfbf9898f6c8cd6f1b86daf0f95092b9717c79
ahmadreza <menatiahmadreza@gmail.com>
2026-08-17 18:34:46 +0330

    Add edit

--- app/ai_editor.py
+++ app/ai_editor.py
@@ -31,7 +31,7 @@ Rules:
 """
 
     response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
+        model="openai/gpt-oss-20b",
         messages=[
             {
                 "role": "user",
--- app/translator.py
+++ app/translator.py
@@ -45,7 +45,7 @@ DESCRIPTION:
 """
 
     response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
+        model="openai/gpt-oss-20b",
         messages=[
             {
                 "role": "user",
